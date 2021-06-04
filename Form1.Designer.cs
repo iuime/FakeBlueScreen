6b540ae5e9e1c01f8d0fd71fa9656a1900eee1d4
贺悦 <heue@foxmail.com>
2021-06-04 22:17:42 +0800

    Init
    上传

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace 模拟蓝屏
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(108, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 249);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":(";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(150, 520);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1567, 78);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "你的电脑遇到问题，需要重新启动。我们将为你重新启动。";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 900);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(505, 890);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1292, 41);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "有关此问题的详细信息和可能的解决方法，请访问 https://www.windows.com/stopcode";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(505, 1002);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(518, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "如果致电路由支持人员，请向他们提供以下信息";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(505, 1079);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(560, 36);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "终止代码：MACHINE-CHECK-EXCEPTION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(2192, 1248);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = ":)";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 模拟蓝屏
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            #if DEBUG == false
+            //Console.WriteLine("禁用");
+            Hook_Start();
+            #endif
+            Thread trd = new Thread(events)
+            {
+                IsBackground = true
+            };
+            trd.Start();
+        }
+
+        private void events()
+        {
+            Thread.Sleep(20000);
+            Hook_Clear();
+            Process.GetCurrentProcess().CloseMainWindow();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            #if DEBUG == false
+            //Console.WriteLine("删除");
+            deleteSelf();
+            #endif
+        }
+
+        private void deleteSelf()
+        {
+            string file = Process.GetCurrentProcess().MainModule.FileName;
+            string bat = "@echo off\n:again\ndel %1\nif exist %1 goto again\ndel %0";
+            File.WriteAllText("0.bat", bat);
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "0.bat";
+            psi.Arguments = file;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(psi);
+        }
+
+
+        #region 屏蔽WIN功能键
+        public delegate int HookProc(int nCode, int wParam, IntPtr lParam);
+        private static int hHook = 0;
+        public const int WH_KEYBOARD_LL = 13;
+
+        //LowLevel键盘截获，如果是WH_KEYBOARD＝2，并不能对系统键盘截取，会在你截取之前获得键盘。 
+        private static HookProc KeyBoardHookProcedure;
+        //键盘Hook结构函数 
+        [StructLayout(LayoutKind.Sequential)]
+        public class KeyBoardHookStruct
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        //设置钩子 
+        [DllImport("user32.dll")]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        //抽掉钩子 
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32.dll")]
+        //调用下一个钩子 
+        public static extern int CallNextHookEx(int idHook, int nCode, int wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string name);
+
+        public static void Hook_Start()
+        {
+            // 安装键盘钩子 
+            if (hHook == 0)
+            {
+                KeyBoardHookProcedure = new HookProc(KeyBoardHookProc);
+                hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyBoardHookProcedure,
+                        GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName), 0);
+                //如果设置钩子失败. 
+                if (hHook == 0)
+                {
+                    Hook_Clear();
+                }
+            }
+        }
+
+        //取消钩子事件 
+        public static void Hook_Clear()
+        {
+            bool retKeyboard = true;
+            if (hHook != 0)
+            {
+                retKeyboard = UnhookWindowsHookEx(hHook);
+                hHook = 0;
+            }
+            //如果去掉钩子失败. 
+            if (!retKeyboard) throw new Exception("UnhookWindowsHookEx failed.");
+        }
+
+        //这里可以添加自己想要的信息处理 
+        private static int KeyBoardHookProc(int nCode, int wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                KeyBoardHookStruct kbh = (KeyBoardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyBoardHookStruct));
+
+                if (kbh.vkCode == 91) // 截获左win(开始菜单键) 
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == 92)// 截获右win 
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == (int)Keys.Escape && (int)Control.ModifierKeys == (int)Keys.Control) //截获Ctrl+Esc 
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == (int)Keys.F4 && (int)Control.ModifierKeys == (int)Keys.Alt) //截获alt+f4 
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == (int)Keys.Tab && (int)Control.ModifierKeys == (int)Keys.Alt) //截获alt+tab
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == (int)Keys.Escape && (int)Control.ModifierKeys == (int)Keys.Control + (int)Keys.Shift) //截获Ctrl+Shift+Esc
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == (int)Keys.Space && (int)Control.ModifierKeys == (int)Keys.Alt) //截获alt+空格 
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == 241) //截获F1 
+                {
+                    return 1;
+                }
+
+                if ((int)Control.ModifierKeys == (int)Keys.Control + (int)Keys.Alt + (int)Keys.Delete)      //截获Ctrl+Alt+Delete 
+                {
+                    return 1;
+                }
+
+                if ((int)Control.ModifierKeys == (int)Keys.Control + (int)Keys.Shift) //截获Ctrl+Shift 
+                {
+                    return 1;
+                }
+
+                if (kbh.vkCode == (int)Keys.Space && (int)Control.ModifierKeys == (int)Keys.Control + (int)Keys.Alt) //截获Ctrl+Alt+空格 
+                {
+                    return 1;
+                }
+            }
+
+            return CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+        public static void TaskMgrLocking(bool bLock)
+        {
+            if (bLock)
+            {
+                try
+                {
+                    RegistryKey r = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true);
+                    r.SetValue("DisableTaskmgr", "1"); //屏蔽任务管理器 
+                }
+                catch
+                {
+                    RegistryKey r = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+                    r.SetValue("DisableTaskmgr", "0");
+                }
+            }
+            else
+            {
+                Registry.CurrentUser.DeleteSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            }
+        }
+        #endregion
+
+        //  调用，这个需要放在主窗体的load方法中
+        //private void Load(object sender, EventArgs e)
+        //{
+        //    Hook_Start();
+        //}
+
+        //卸载
+        /*private void FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hook_Clear();
+        }*/
+    }
+}
